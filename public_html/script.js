61b9a308792877c12151b73680a8442944dcfd3e
waY <vkfixxed@gmail.com>
2026-02-07 14:33:02 +0300

    Update script.js

--- public_html/script.js
+++ public_html/script.js
@@ -1,1 +1,20 @@
 
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+async function loadInventory() {
+    const chatId = tg.initDataUnsafe.user.id;
+    // Запрос к твоему новому API
+    const response = await fetch(`/api/inventory?chat_id=${chatId}`);
+    const data = await response.json();
+    
+    const container = document.getElementById('inventory');
+    container.innerHTML = data.map(item => `
+        <div class="item-card">
+            <span>${item.name}</span>
+            <b>x${item.amount}</b>
+        </div>
+    `).join('');
+}
+
+loadInventory();
